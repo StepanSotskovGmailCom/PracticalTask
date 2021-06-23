28b860cec70bc6b7525e0b07148920162ab88a0a
Stepan Sotskov <stepan.sotskov@gmail.com>
2021-06-23 12:35:19 +0300

    Practical task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     public static double areaOfTriangle(double a, double b) {
-        if(a<=0 || b<=0){
+        if (a <= 0 || b <= 0) {
             throw new IllegalArgumentException("Values should be positive");
         }
         return (a * b) / 2;
@@ -21,19 +21,19 @@ public class Main {
     }
 
     public static String personalizedGreeting(String name, String friendsName) {
-        if(isNullorBlank(name) || isNullorBlank(friendsName)){
+        if (isNullorBlank(name) || isNullorBlank(friendsName)) {
             throw new IllegalArgumentException("Values must be not empty");
         }
         return String.format("Hello %s! Your friend %s invited you to join this app!", name, friendsName);
     }
 
-    private static boolean isNullorBlank (String value){
+    private static boolean isNullorBlank(String value) {
         return value == null || value.length() == 0;
     }
 
     public static void primeNumbers(int x) {
 
-        if(x < 1) {
+        if (x < 1) {
             throw new IllegalArgumentException("Value should be greater than 0");
         }
         List<Integer> primes = generatePrimes(x);
@@ -57,7 +57,7 @@ public class Main {
 
 
     public static boolean isPrime(int number) {
-        for (int i = 2; i*i <= number; i++) {
+        for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 return false;
             }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,67 +8,64 @@ import static org.junit.Assert.*;
 public class MainTest {
 
     @Test
-    public void shouldCalculateArea(){
-        double area = Main.areaOfTriangle(5,2);
-        assertEquals(5,area,0);
+    public void shouldCalculateArea() {
+        double area = Main.areaOfTriangle(5, 2);
+        assertEquals(5, area, 0);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowException(){
-        Main.areaOfTriangle(-5,2);
+    public void shouldThrowException() {
+        Main.areaOfTriangle(-5, 2);
     }
 
     @Test
-    public void shouldReturnTrueIfOdd(){
+    public void shouldReturnTrueIfOdd() {
         assertTrue(Main.isOdd(1));
     }
 
     @Test
-    public void shouldReturnFalseIfEven(){
+    public void shouldReturnFalseIfEven() {
         assertFalse(Main.isOdd(2));
     }
 
-     @Test
-    public void shouldReturnCorrectString(){
-        String expected  = "Hello Linda! Your friend Zane invited you to join this app!";
-        String result = Main.personalizedGreeting("Linda","Zane");
-        assertEquals(expected,result);
+    @Test
+    public void shouldReturnCorrectString() {
+        String expected = "Hello Linda! Your friend Zane invited you to join this app!";
+        String result = Main.personalizedGreeting("Linda", "Zane");
+        assertEquals(expected, result);
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionIfNameIsEmpty() {
-        Main.personalizedGreeting("","");
+        Main.personalizedGreeting("", "");
     }
 
-     @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionIfNameIsNull() {
-        Main.personalizedGreeting(null,null);
-    }
-
-      @Test
-    public void shouldReturnTrueIfPrime(){
-       assertTrue(Main.isPrime(7));
+        Main.personalizedGreeting(null, null);
     }
 
     @Test
-    public void shouldReturnFalseIfNotPrime(){
-       assertFalse(Main.isPrime(6));
+    public void shouldReturnTrueIfPrime() {
+        assertTrue(Main.isPrime(7));
     }
 
     @Test
-    public void shouldGeneratePrimes(){
-        List<Integer> expected  = Arrays.asList(2, 3, 5, 7);
+    public void shouldReturnFalseIfNotPrime() {
+        assertFalse(Main.isPrime(6));
+    }
+
+    @Test
+    public void shouldGeneratePrimes() {
+        List<Integer> expected = Arrays.asList(2, 3, 5, 7);
         List<Integer> result = Main.generatePrimes(4);
-        assertEquals(expected,result);
+        assertEquals(expected, result);
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionIfRequestedAmountInvalid() {
         Main.primeNumbers(0);
     }
-
-
-
 
 
 }
